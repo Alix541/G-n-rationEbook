c95c455df17fc5fccd90df7278411e4f99cf180b
Alix541 <alix.caperan@outlook.fr>
2025-06-12 17:54:47 +0100

    Géné Trame+Personnages
    J'ai finie la génération de la trame, la génération des personnages, et je commance la génération des shema narratif.

---
+++ ClassPers.py
@@ -0,0 +1,23 @@
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+class Perssonage(BaseModel):
+    """Description du personnage"""
+    PersonnagePrincipal : bool = Field(description="Détermine si le personnage est un personnage principal ou pas")
+    PersonnageSecondaireMajeur : bool = Field(description="amis, mentors, antagonistes secondaires — ils reviennent souvent et influencent directement l’intrigue ou le héros.")
+    PersonnageSecondaireMineur : bool = Field(description="réceptionniste, chauffeur, professeur, etc. — présents dans 1 à 2 scènes, mais servent l’ambiance ou le réalisme.")
+    Prenom : str = Field(description="Le prenom du personnage ")
+    Nom : Optional[str] = Field(description="Le nom du personnage ")
+    Surnom : Optional[str] = Field(description="Un surnom par lequel on appelle le personnages")
+    Age : int = Field(description="L'âge du personnage ")
+    Faiblesse : str = Field(description="Ce qui rend le personnage vulnérable : difficultés du quotidien, des doutes, des contradictions, des limite, des faiblesses physiques etc...")
+    Force : str = Field(description="Ce qui diférencie notre super-héros du reste des personnages, ce qui le rends particulier et fort")
+    Caractere : str = Field(description="Est ce qu'il est nerveux, gentil, méchant, attentionner, dur, fou etc...")
+    Mission : str = Field(description="Le but du personnages dans l'histoire")
+    Amis : Optional[str] = Field(description="Les compagnons de routes du personnages, les personnes qu'ils aiment bien, ça famille, amis etc...")
+    Ennemie : Optional[str] = Field(description="Les personnes que le héro n'aime pas, qui se mette sur ça route, qui sont contre lui")
+    DescriptionPhysique : str = Field(description="Une courte description physique du personnages, en comprenant les habits et la morphologie")
+
+class ListePersonnages(BaseModel):
+    """Une liste de personnage pour l'histoire"""
+    personnages: List[Perssonage]
--- EbookGene.py
+++ EbookGene.py
@@ -1,5 +1,13 @@
 import getpass
 import os
+import categorie
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.agents import Tool, initialize_agent
+from langchain.schema import AgentAction, AgentFinish
+from langchain.agents.agent_types import AgentType
+import categorie
+import ClassPers
 
 os.environ["LANGSMITH_TRACING"] = "true"
 if "LANGSMITH_API_KEY" not in os.environ:
@@ -8,3 +16,57 @@ if "LANDSMITH_PROJECT" not in os.environ:
     os.environ["LANGSMITH_PROJECT"]="default"
 if "OPENAI_API_KEY" not in os.environ:
     os.environ["OPENAI_API_KEY"]="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"
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
+
+scenario=categorie.tirer_scenario()
+
+#Génération de la Trame, en 5 lignes
+
+message = [
+    SystemMessage("Ecrit la trame principal en un paragraphe de 5 lignes maximum"),
+    HumanMessage(f"Ecrit une trame pour ce scenario : {scenario}")
+]
+
+Trame = model.invoke(message).content
+print(Trame)
+
+#Génération des personnages
+
+Pers_struct = model.with_structured_output(ClassPers.ListePersonnages)
+
+message = [
+    SystemMessage(content="Tu es un assistant narratif spécialisé en fiction."),
+    SystemMessage(content="Ta mission est de générer des personnages cohérents avec l'univers donné."),
+    SystemMessage(content=f"Scenario : {scenario}"),
+    SystemMessage(content=f"Trame : {Trame}"),
+    HumanMessage(content="Génère un personnage principal, selon les besoins entre 3 et 5 personnages secondaires majeurs et entre 1 et 5 personnages secondaires mineurs")
+]
+
+PersonnageDict = Pers_struct.invoke(message).model_dump()
+print(PersonnageDict)
+
+#Génération du scenario des péripécie
+
+message = [
+    
+]
+
+"""
+tools=[
+    Tool(
+        name="Le scenario a utiliser",
+        func=scenario,
+        description="Le scenario pour construire la trame"
+    )
+]
+
+agent = initialize_agent(
+    tools,
+    model,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
+
+Trame = agent.run("Génère une trame principale de roman à partir d'un scénario aléatoire.")
+"""
