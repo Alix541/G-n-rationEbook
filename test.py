3dd1fd0021fca88905a768ad533c5e0994697ed8
Alix541 <alix.caperan@outlook.fr>
2025-06-15 13:20:06 +0100

    Géné du roman avec chap
    J'ai fini la première génération du squelette de l'histoire : GénéShemaNarratif -> GénéMonde -> GénéPersonnage -> GénéShemaComplémentaire -> GénéChapitre ->GénéTitre

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+import ClassTram
+import getpass
+import os
+import inspect
 import categorie
 
-scenario=categorie.tirer_scenario()
+struct_roman=categorie.tirer_scenario()
 
-print(scenario["schema_principal"])
+SCHEMA_PRINCIPAL_CLASSES = {
+    name: cls
+    for name, cls in inspect.getmembers(ClassTram, inspect.isclass)
+    if cls.__module__ == ClassTram.__name__
+}
+
+schema_principal_class = SCHEMA_PRINCIPAL_CLASSES[struct_roman["schema_principal"]]
+#print("Classes trouvées dans ClassTram :", list(SCHEMA_PRINCIPAL_CLASSES.keys()))
+#print("Classe principale :", schema_principal_class)
+print(SCHEMA_PRINCIPAL_CLASSES)
